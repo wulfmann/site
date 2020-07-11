b0fe46f5d8c8d6f6d7e18b7f6c410699a8022045
Joe Snell <joepsnell@gmail.com>
2020-07-11 18:26:47 -0500

    update

--- src/components/layouts/default.tsx
+++ src/components/layouts/default.tsx
@@ -3,13 +3,13 @@ import Footer from '../Footer';
 
 function DefaultLayout({ children }) {
   return (
-    <>
+    <div className="default-layout">
       <Header />
       
       <main>{children}</main>
       
       <Footer />
-    </>
+    </div>
   );
 }
 
