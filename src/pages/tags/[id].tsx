4330ec3cef4490a82ccd26c33f1abfc27cdc9cf8
Joe Snell <joepsnell@gmail.com>
2020-07-12 10:57:33 -0500

    add tags pages

---
+++ src/pages/tags/[id].tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import Layout from '../components/layouts/default';
+
+function Tag({ tag }) {
+  return (
+    <>
+      <Head>
+        <title>Joe Snell - {tag} Posts</title>
+      </Head>
+      
+      <div>
+        {tag} posts
+      </div>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { tag: 'test' }
+      }
+    ],
+    fallback: false
+  };
+}
---
+++ src/pages/tags/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../components/layouts/default';
+
+function Tags({ children }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Joe Snell - Tags</title>
+        <meta name="description" content="Joe Snell - Tags" />
+      </Head>
+      
+      <div>
+        <Link href={`/tags/${tag}`}>
+          <a>{tag}</a>
+        </Link>
+      </div>
+    </Layout>
+  );
+}
+
+export default Tags;
