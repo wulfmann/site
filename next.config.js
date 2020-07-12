896fa3d7ea289aee10202acfb02761c1bc16c248
Joe Snell <joepsnell@gmail.com>
2020-07-12 10:14:01 -0500

    add post

---
+++ next.config.js
@@ -0,0 +1,6 @@
+const withMdxEnhanced = require('next-mdx-enhanced')
+
+module.exports = withMdxEnhanced({
+  layoutPath: 'src/components/layouts',
+  defaultLayout: true
+})({});
---
+++ src/components/layouts/post.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Header from '../Header';
+import Footer from '../Footer';
+
+function Post(frontMatter) {
+  return({ children }) (
+    <div className="default-layout">
+      <Head>
+        <title>Joe Snell - {frontMatter.title}</title>
+        <meta name="description" content="Joe Snell - {frontMatter.description}" />
+      </Head>
+      <Header />
+      
+      <main>{children}</main>
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default Post;
