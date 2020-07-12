8a19a7ce11a9217717619f2cdff9e62ee6581674
Joe Snell <joepsnell@gmail.com>
2020-07-12 07:31:09 -0500

    sorted

--- src/pages/oss.tsx
+++ src/pages/oss.tsx
@@ -22,8 +22,43 @@ function OSS() {
       title: 'auto-cdk',
       description: '',
       href: 'https://github.com/wulfmann/auto-cdk'
+    },
+    {
+      title: 'am-i-alone',
+      description: '',
+      href: 'https://github.com/wulfmann/am-i-alone'
+    },
+    {
+      title: 'create-cdk-app',
+      description: '',
+      href: 'https://github.com/cdk-tools/create-cdk-app'
+    },
+    {
+      title: 'ircra',
+      description: '',
+      href: 'https://github.com/wulfmann/ircra'
+    },
+    {
+      title: 'cdk-experiments',
+      description: '',
+      href: 'https://github.com/wulfmann/cdk-experiments'
+    },
+    {
+      title: 'pin-latest',
+      description: '',
+      href: 'https://github.com/wulfmann/pin-latest'
+    },
+    {
+      title: 'use-okta',
+      description: '',
+      href: 'https://github.com/wulfmann/use-okta'
+    },
+    {
+      title: 'jtx',
+      description: '',
+      href: 'https://github.com/wulfmann/jtx'
     }
-  ];
+  ].sort((a, b) => a.name > b.name);
 
   return (
     <>
