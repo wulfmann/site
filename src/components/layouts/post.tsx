a85539c9b10dc95816c95b0942ec00a83d05cd41
Joe Snell <joepsnell@gmail.com>
2020-07-12 10:17:27 -0500

    typo

--- src/components/layouts/post.tsx
+++ src/components/layouts/post.tsx
@@ -3,7 +3,7 @@ import Header from '../Header';
 import Footer from '../Footer';
 
 function Post(frontMatter) {
-  return({ children }) (
+  return ({ children }) => (
     <div className="default-layout">
       <Head>
         <title>Joe Snell - {frontMatter.title}</title>
