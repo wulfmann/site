becd2ec62df754760074fdba325ef206dbe4353b
Joe Snell <joepsnell@gmail.com>
2020-07-19 10:25:58 -0500

    add dribbble link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ function Footer() {
       <ul>
         <li><a href="https://github.com/wulfmann">GitHub</a></li>
         <li><a href="https://twitter.com/wlfmnn">Twitter</a></li>
+        <li><a href="https://dribbble.com/wulfmann">Dribbble</a></li>
         <li><a href="mailto:joe@snell.im">joe@snell.im</a></li>
       </ul>
     </footer>
